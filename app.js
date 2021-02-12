18f81b29cede60ad2aea2927567caaa4144cc25c
Sergio <srosduran@gmail.com>
2021-02-12 11:20:09 +0100

    holi

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const logger = require('morgan');
 const path = require('path');
 const createError  = require('http-errors');
 
-const passport = require('passport');
+const passport = require('passport'); 
 const cors = require('./configs/cors.config')
 const session = require('./configs/session.config');
 
